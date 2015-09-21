52aa3ac604f9664fff3523dfc957555288bd3d74
ilyes hammadi <hammadiilyesahmed@gmail.com>
2015-09-21 21:44:07 +0100

    set up the Recycle View and it's work youpiiii

---
+++ app/src/main/java/com/cortexia/taref/fragment/ItemsRecycleAdapter.java
@@ -0,0 +1,71 @@
+package com.cortexia.taref.fragment;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.cortexia.taref.R;
+import com.cortexia.taref.model.Item;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by ilyes on 28/08/15.
+ */
+public class ItemsRecycleAdapter extends RecyclerView.Adapter<ItemsRecycleAdapter.MyViewHolder> {
+
+    private LayoutInflater mInflater;
+    private List<Item> mData = Collections.emptyList();
+
+    public ItemsRecycleAdapter(Context context, List<Item> data) {
+        mInflater = LayoutInflater.from(context);
+        mData = data;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.item_list, parent, false);
+        MyViewHolder holder = new MyViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // Getting the data from the data array
+        Item object = mData.get(position);
+
+        // Setting the views to reflect the data we need to display
+        holder.nameTV.setText(object.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        // Set the view ref here
+        TextView nameTV;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+
+            // Get refrence to view ( findViewByid())
+            nameTV = (TextView) itemView.findViewById(R.id.item_name_tv);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            // Setup the click on a single item from the list
+        }
+    }
+
+}
--- app/src/main/java/com/cortexia/taref/fragment/RecycleItemsFragment.java
+++ app/src/main/java/com/cortexia/taref/fragment/RecycleItemsFragment.java
@@ -3,11 +3,17 @@ package com.cortexia.taref.fragment;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.cortexia.taref.R;
+import com.cortexia.taref.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,17 +21,50 @@ import com.cortexia.taref.R;
 public class RecycleItemsFragment extends android.support.v4.app.Fragment {
 
 
+    private View mRoot;
+    private RecyclerView mListItemsRV;
+
+    private List<Item> fakeData = new ArrayList<>();
+
     public RecycleItemsFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recycle_items, container, false);
+        mRoot = inflater.inflate(R.layout.fragment_recycle_items, container, false);
+
+        // Set up Recycle View
+        mListItemsRV = (RecyclerView) mRoot.findViewById(R.id.list_item_rv);
+        mListItemsRV.setLayoutManager(setUpLayoutManager());
+        setFakeData();
+        mListItemsRV.setAdapter(new ItemsRecycleAdapter(getActivity(), fakeData));
+
+        return mRoot;
     }
 
+    private GridLayoutManager setUpLayoutManager() {
+        GridLayoutManager manager = new GridLayoutManager(getActivity(), 2);
+        manager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return (position % 3 == 0 ? 2 : 1);
+            }
+        });
+        return manager;
+    }
+
+
+    private void setFakeData() {
+
+        for (int i = 0; i < 20; i++) {
+            Item item = new Item();
+            item.setName("Hello world");
+            fakeData.add(item);
+        }
+
+    }
 
 }
