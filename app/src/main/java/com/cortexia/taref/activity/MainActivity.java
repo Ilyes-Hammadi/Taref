cf5594689918429f4dfc27b22027452c72af8c5b
ilyes hammadi <hammadiilyesahmed@gmail.com>
2015-09-24 15:49:10 +0100

    adedd search opton in the ToolBar

--- app/src/main/java/com/cortexia/taref/activity/MainActivity.java
+++ app/src/main/java/com/cortexia/taref/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.cortexia.taref.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -10,6 +12,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +20,9 @@ import android.view.View;
 import com.cortexia.taref.R;
 import com.cortexia.taref.adapter.SectionAdapter;
 import com.github.saiff35.livingtabs.LivingTabsLayout;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -28,13 +34,12 @@ public class MainActivity extends AppCompatActivity
     private SectionAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
     private DrawerLayout mDrawer;
+    private MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         setUpToolbar();
 
@@ -44,7 +49,43 @@ public class MainActivity extends AppCompatActivity
 
         setUpNavigationDrawer();
 
+        setUpSearchBar();
+
+
     }
+
+    private void setUpSearchBar() {
+        searchView = (MaterialSearchView) findViewById(R.id.search);
+
+        searchView.setVoiceSearch(true);
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Do some magic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //Do some magic
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+            }
+        });
+    }
+
 
     private void setUpTabs() {
         // Create the adapter that will return a fragment for each of the three
@@ -101,6 +142,10 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+
         return true;
     }
 
@@ -140,4 +185,21 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (matches != null && matches.size() > 0) {
+                String searchWrd = matches.get(0);
+                if (!TextUtils.isEmpty(searchWrd)) {
+                    searchView.setQuery(searchWrd, false);
+                }
+            }
+
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
